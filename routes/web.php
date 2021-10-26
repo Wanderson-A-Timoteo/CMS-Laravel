43d27d3662d826e6ebcfc110b55d665a49bb9309
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2021-10-26 10:00:48 -0400

    add separação do sistema em painel admin com auth e site, tbm config rotas e add AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', 'Site\HomeController@index');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('painel')->group(function () {
+    Route::get('/', 'Admin\HomeController@index')->name('admin');
+    Route::get('login', 'Admin\Auth\LoginController@index')->name('login');
 });
